a1dc789afeb38d2b5b73de4be1a8712934ecf6a9
Mauricio Gamboa <magalocr@gmail.com>
2015-02-06 15:55:13 -0600

    1170

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var myApp = angular.module('LM', ['duScroll']);
+
+myApp.directive('owlSingle', [function () {
+  return {
+    restrict: 'A',
+
+    link: function (scope, element) {
+      element.owlCarousel({
+        autoPlay: true,
+        singleItem: true
+      });
+    }
+  };
+}]);
+
+myApp.directive('owlBrands', [function () {
+  return {
+    restrict: 'A',
+
+    link: function (scope, element) {
+      element.owlCarousel({
+        items: 6,
+        autoPlay: true,
+        pagination: false
+      });
+    }
+  };
+}]);
